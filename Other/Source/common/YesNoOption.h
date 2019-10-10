8083fa6bae0f90feed602b67aaef5f7328d01e09
Jacek Banaszczyk <jacek.banaszczyk@gmail.com>
2019-10-10 22:04:24 +0200

    Resharpered

--- Other/Source/common/YesNoOption.h
+++ Other/Source/common/YesNoOption.h
@@ -9,7 +9,7 @@
 
  /******************************************************************************
  *
- * class pApps::YesNoOption converts strings to boolan
+ * class pApps::YesNoOption converts strings to boolean
  *
  * Supported strings: "Yes", "No", "true", "false", "1", "0"
  *
@@ -26,47 +26,39 @@
 namespace p_apps {
 
 	class YesNoOption {
-	private:
-		template<typename T>
-		struct fuLess : std::binary_function < T, T, bool > {
-			bool operator() (const T& s1, const T& s2) const {
+		template <typename T>
+		struct fuLess : std::binary_function<T, T, bool> {
+			bool operator()(const T& s1, const T& s2) const {
 				return boost::ilexicographical_compare(s1, s2);
 			}
 		};
-		std::map< std::tstring, bool, fuLess<std::tstring>> _values;
+
+		std::map<std::tstring, bool, fuLess<std::tstring>> _values;
 
 		bool getOption(const std::tstring& key, const bool defValue) const {
-			auto itValues = _values.find(key);
+            const auto itValues = _values.find(key);
 			if (_values.end() == itValues)
 				return defValue;
 			return itValues->second;
-		};
+		}
 
 	public:
 		YesNoOption() : _values({
-			{ _T("yes"), true },
-			{ _T("true"), true },
-			{ _T("1"), true },
-			{ _T("no"), false },
-			{ _T("false"), false },
-			{ _T("0"), false }
-			}) { }
-
-		~YesNoOption() {};
-
-		void addString(const std::tstring& key, const bool value) {
-			_values[key] = value;
+			{_T("yes"), true}, {_T("true"), true}, {_T("1"), true}, {_T("no"), false}, {_T("false"), false}, {_T("0"), false}
+			}) {
 		}
 
-		bool operator()(const std::tstring& key, const bool defValue = true) {
-			return getOption(key, defValue);
-		};
+		~YesNoOption() = default;
 
-		bool operator()(const boost::optional<std::tstring>& key, const bool defValue = true) {
+        bool operator()(const std::tstring& key, const bool defValue = true) const {
+			return getOption(key, defValue);
+		}
+
+		bool operator()(const boost::optional<std::tstring>& key, const bool defValue = true) const {
 			if (!key)
 				return defValue;
 			return getOption(key.get(), defValue);
-		};
+		}
 
 	};
 }
